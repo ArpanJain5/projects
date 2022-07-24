470230fcae88ba0c8d1e71e97453265dc690bc50
DEADLyAJ <arpanrewari@gmail.com>
2022-07-24 15:56:15 +0530

    all works

---
+++ HIbernateDemo/src/main/java/OneToMany/Ans.java
@@ -0,0 +1,33 @@
+package OneToMany;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Ans {
+	@Id
+	private int id;
+	private String answer;
+	@ManyToOne
+	private Que question;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswer() {
+		return answer;
+	}
+	public void setAnswer(String answer) {
+		this.answer = answer;
+	}
+	public Que getQuestion() {
+		return question;
+	}
+	public void setQuestion(Que question) {
+		this.question = question;
+	}
+	
+}
